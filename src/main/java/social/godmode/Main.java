697070f88cb1b1df6ad3166935531052ba91b607
godmodeD <djlevy26@gmail.com>
2023-03-19 01:20:15 -0400

    don't need it

--- src/main/java/social/godmode/Main.java
+++ src/main/java/social/godmode/Main.java
@@ -1,14 +1,10 @@
 package social.godmode;
 
 import lombok.Getter;
-import me.nickrest.command.CommandManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-import social.godmode.commands.PingCommand;
-
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -21,8 +17,6 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         loadConfig();
         JDA jda = JDABuilder.createDefault(token).build();
-        CommandManager commandManager = new CommandManager(jda);
-        commandManager.register(new PingCommand());
         jda.awaitReady();
         gpt = new GPT(apiKey);
         // register eventHandler as a listener
