8acd043c7d9fd1a59cd97b470956ff81c4926739
godmodeD <djlevy26@gmail.com>
2023-03-18 22:29:15 -0400

    fix?

--- src/main/java/social/godmode/Main.java
+++ src/main/java/social/godmode/Main.java
@@ -4,45 +4,40 @@ import lombok.Getter;
 import me.nickrest.command.CommandManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import social.godmode.commands.PingCommand;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
+
+    private static String token;
+    private static String apiKey;
     @Getter
     private static GPT gpt;
     public static void main(String[] args) throws InterruptedException {
-        JDA jda = JDABuilder.createDefault(getToken()).build();
+        loadConfig();
+        JDA jda = JDABuilder.createDefault(token).build();
         CommandManager commandManager = new CommandManager(jda);
         commandManager.register(new PingCommand());
         jda.awaitReady();
-        gpt = new GPT(getAPIKey());
+        gpt = new GPT(apiKey);
         // register eventHandler as a listener
         jda.addEventListener(new EventHandler());
     }
-    private static String getToken() {
-        StringBuilder sb = new StringBuilder();
-        try(FileReader reader = new FileReader("./token.txt")) {
-            int c;
-            while((c = reader.read()) != -1) {
-                sb.append((char) c);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return sb.toString();
-    }
 
-    private static String getAPIKey() {
-        StringBuilder sb = new StringBuilder();
-        try(FileReader reader = new FileReader("./api.txt")) {
-            int c;
-            while((c = reader.read()) != -1) {
-                sb.append((char) c);
-            }
+    private static void loadConfig() {
+        try {
+            FileReader reader = new FileReader("./config.json");
+            JSONObject config = new JSONObject(new JSONTokener(reader));
+            reader.close();
+            token = config.getString("discord_token");
+            apiKey = config.getString("openai_token");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return sb.toString();
     }
 }
